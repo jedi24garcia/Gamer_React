f934b7328d92b2f26164126f7cec329dc269a790
jedi24garcia <jedi24garcia@gmail.com>
2023-09-27 22:39:34 +1300

    updated

--- src/Next-Page.js
+++ src/Next-Page.js
@@ -4,11 +4,11 @@ import './Next-Page.css';
 function Home1() {
   return (
     <header>
-      <h1>GamerTech</h1>
+      <h1>GamerTech Consoles</h1>
       <nav>
         <Link to="">Home</Link>
         <Link to="/menu">Menu</Link>
-        <Link to="/logIn">Log In/Sign Up</Link>
+        <Link to="/logIn">Sign Up</Link>
         <Link to="/contacts">Contact</Link>
       </nav>
     </header>
--- src/log-in.js
+++ src/log-in.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './log-in.css';
 
-function UserlogIn () {
-    return (
-        <h1>Log in or Sign Up</h1>
-    );
+function UserlogIn() {
+  const [email, EmailSet] = useState('');
+  const [password, PasswordSet] = useState('');
+  const [PasswordRepeat, RepeatPasswordSet] = useState('');
+
+  const SignConfig = () => {
+    if (email && password && PasswordRepeat && password === PasswordRepeat) {
+      // All fields are filled, and passwords match
+      alert('Account created! HAPPY GAMING!');
+    } else {
+      alert('Please fill out all fields, and make sure the passwords match.');
+    }
+  };
+
+  return (
+    <div className="SignUpSign">
+      <h1>Sign Up</h1>
+      <div className="SignForm">
+        <label for="email"><b>Email</b></label>
+        <input type="text" placeholder="Please enter email" name="email" value={email} onChange={(e) => EmailSet(e.target.value)}/>
+        <label for="password"><b>Password</b></label>
+        <input type="text" placeholder="Please enter password" name="password" value={password} onChange={(e) => PasswordSet(e.target.value)}/>
+        <label for="repeatPassword"><b>Repeat Password</b></label>
+        <input type="text" placeholder="Please repeat password" name="repeatPassword" value={PasswordRepeat} onChange={(e) => RepeatPasswordSet(e.target.value)}/>
+        <button onClick={SignConfig}>Enter</button>
+      </div>
+    </div>
+  );
 }
 
 export default UserlogIn;
