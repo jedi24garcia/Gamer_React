bfa06436862d17665ddb454bc36fe11909c8463b
jedi24garcia <jedi24garcia@gmail.com>
2023-09-29 03:21:15 +1300

    updated some files

--- src/genre/freetoplay.js
+++ src/genre/freetoplay.js
@@ -1,9 +1,75 @@
 import React from 'react';
+import './freetoplay.css';
 
-function FreeToPlay () {
+const WorldOfTanks = () => {
+    alert("Please download using winget install “APP-NAME-1” -e && winget install “APP-NAME-2” -e code 11111 to install");
+};
+const DotaUnderlords = () => {
+    alert("Please download using winget install “APP-NAME-1” -e && winget install “APP-NAME-2” -e code 11112 to install");
+};
+const MIR4 = () => {
+    alert("Please download using winget install “APP-NAME-1” -e && winget install “APP-NAME-2” -e code 11113 to install");
+};
+const BlessUnleashed = () => {
+    alert("Please download using winget install “APP-NAME-1” -e && winget install “APP-NAME-2” -e code 11114 to install");
+};
+const RuneScape = () => {
+    alert("Please download using winget install “APP-NAME-1” -e && winget install “APP-NAME-2” -e code 11115 to install");
+};
+const Dota2 = () => {
+    alert("Please download using winget install “APP-NAME-1” -e && winget install “APP-NAME-2” -e code 11116 to install");
+};
+const StarWars = () => {
+    alert("Please download using winget install “APP-NAME-1” -e && winget install “APP-NAME-2” -e code 11117 to install");
+};
+const TorchLight = () => {
+    alert("Please download using winget install “APP-NAME-1” -e && winget install “APP-NAME-2” -e code 11118 to install");
+};
+const InfiniteHalo = () => {
+    alert("Please download using winget install “APP-NAME-1” -e && winget install “APP-NAME-2” -e code 11111 to install");
+};
+const FarLight84 = () => {
+    alert("Please download using winget install “APP-NAME-1” -e && winget install “APP-NAME-2” -e code 11112 to install");
+};
+const Wizard101 = () => {
+    alert("Please download using winget install “APP-NAME-1” -e && winget install “APP-NAME-2” -e code 11113 to install");
+};
+const RuneOfMagic = () => {
+    alert("Please download using winget install “APP-NAME-1” -e && winget install “APP-NAME-2” -e code 11114 to install");
+};
+const Century101 = () => {
+    alert("Please download using winget install “APP-NAME-1” -e && winget install “APP-NAME-2” -e code 11115 to install");
+};
+const WurmOnline = () => {
+    alert("Please download using winget install “APP-NAME-1” -e && winget install “APP-NAME-2” -e code 11116 to install");
+};
+const WarLander = () => {
+    alert("Please download using winget install “APP-NAME-1” -e && winget install “APP-NAME-2” -e code 11117 to install");
+};
+
+function FreePlay() {
     return (
-        <h1>Free to Play</h1>
-    )
+        <div className="freeTitle">
+            <h1>Free to Play games</h1>
+            <div className="freeButtons">
+                <button onClick={WorldOfTanks}>World of Tanks</button>
+                <button onClick={DotaUnderlords}>Dota Underlords</button>
+                <button onClick={MIR4}>MIR4</button>
+                <button onClick={BlessUnleashed}>Bless Unleashed</button>
+                <button onClick={RuneScape}>Runescape</button>
+                <button onClick={Dota2}>Dota 2</button>
+                <button onClick={StarWars}>Star Wars</button>
+                <button onClick={TorchLight}>Torchlight</button>
+                <button onClick={InfiniteHalo}>Infinite Halo</button>
+                <button onClick={FarLight84}>Farlight84</button>
+                <button onClick={Wizard101}>Wizard 101</button>
+                <button onClick={RuneOfMagic}>Runes Of Magic</button>
+                <button onClick={Century101}>Century 101</button>
+                <button onClick={WurmOnline}>Wurm Online</button>
+                <button onClick={WarLander}>Warlander</button>
+            </div>
+        </div>
+    );
 }
 
-export default FreeToPlay;
+export default FreePlay;
